91d4b2f42f91cf9a5a0de68909343f7fa7b2549e
semroeten <sem.roeten@gmail.com>
2024-12-29 00:16:19 +0100

    Add setup and template

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+import os.path
+
+
+def main():
+    year = input("Year: ")
+
+    directory = os.path.dirname(os.path.abspath(__file__))
+
+    with open(os.path.join(directory, "template.py")) as file:
+        template = file.read()
+
+    for i in range(1, 26):
+        parts = ("a", "b") if i < 25 else ("a",)
+
+        for part in parts:
+            day_directory = os.path.join(directory, f"{year}", f"day{i}_{part}")
+
+            if os.path.exists(day_directory):
+                continue
+
+            os.makedirs(day_directory)
+
+            open(os.path.join(day_directory, "puzzle.txt"), "w").close()
+            with open(os.path.join(day_directory, "main.py"), "w") as file:
+                file.write(template)
+
+
+if __name__ == "__main__":
+    main()
---
+++ template.py
@@ -0,0 +1,11 @@
+def get_():
+    with open("puzzle.txt") as puzzle:
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
