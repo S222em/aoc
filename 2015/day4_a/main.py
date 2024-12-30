b664721ce30cc9032d1e8b9356475a74ea067fff
semroeten <sem.roeten@gmail.com>
2024-12-30 12:31:46 +0100

    2015 day 4

---
+++ 2015/day4_a/main.py
@@ -0,0 +1,41 @@
+import hashlib
+
+
+def get_secret_key():
+    """
+    Gets the secret key from the puzzle
+    :return:
+    """
+    with open("puzzle.txt") as puzzle:
+        secret_key = puzzle.read().strip()
+
+    return secret_key
+
+
+def find_number(secret_key):
+    """
+    Finds the number with which the secret_key + number hashed with md5
+    has 5 leading zeros.
+    :param secret_key:
+    :return:
+    """
+    i = 0
+
+    while True:
+        hash = hashlib.md5(f"{secret_key}{i}".encode()).hexdigest()
+        if hash.startswith("0" * 5):
+            return i
+
+        i += 1
+
+
+def main():
+    secret_key = get_secret_key()
+
+    number = find_number(secret_key)
+
+    print(number)
+
+
+if __name__ == "__main__":
+    main()
