1d64d167a8dd36cdaebfdae8dda7bbce7304869b
semroeten <sem.roeten@gmail.com>
2025-08-18 14:48:18 +0200

    2015 day 8

--- 2015/day8_a/main.py
+++ 2015/day8_a/main.py
@@ -1,15 +1,45 @@
 from os import path
 
-def get_():
+def get_lines():
     parent = path.abspath(path.dirname(__file__))
 
     with open(path.join(parent, "puzzle.txt")) as puzzle:
-        pass
+        lines = puzzle.readlines()
 
+    return lines
+
+def get_code_char_count(line):
+    return len(line)
+
+def get_memory_char_count(line):
+    count = 0
+    i = 1
+
+    while i < len(line) - 1:
+        if line[i] == "\\" and line[i + 1] in ["\"", "\\", "x"]:
+            i += 4 if line[i + 1] == "x" else 2
+        else:
+            i += 1
+
+        count += 1
+
+    return count
+
+def get_char_difference(lines):
+    total = 0
+
+    for line in lines:
+        total += get_code_char_count(line)
+        total -= get_memory_char_count(line)
+
+    return total
 
 def main():
-    pass
+    lines = get_lines()
 
+    difference = get_char_difference(lines)
+
+    print(difference)
 
 if __name__ == "__main__":
     main()
--- 2015/day8_b/main.py
+++ 2015/day8_b/main.py
@@ -1,15 +1,43 @@
 from os import path
 
-def get_():
+def get_lines():
     parent = path.abspath(path.dirname(__file__))
 
     with open(path.join(parent, "puzzle.txt")) as puzzle:
-        pass
+        lines = puzzle.readlines()
 
+    return lines
+
+def get_literal_char_count(line):
+    return len(line)
+
+def get_encoded_char_count(line):
+    # start at 2 as "" is added
+    count = 2
+
+    for char in line:
+        if char in ["\\", "\""]:
+            count += 1
+
+        count += 1
+
+    return count
+
+def get_char_difference(lines):
+    total = 0
+
+    for line in lines:
+        total += get_encoded_char_count(line)
+        total -= get_literal_char_count(line)
+
+    return total
 
 def main():
-    pass
+    lines = get_lines()
 
+    difference = get_char_difference(lines)
+
+    print(difference)
 
 if __name__ == "__main__":
     main()
