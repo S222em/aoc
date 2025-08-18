6858ec83372854fb913a66d742ef45509b90998f
semroeten <sem.roeten@gmail.com>
2025-08-18 13:57:36 +0200

    2015 day 7

--- 2015/day7_a/main.py
+++ 2015/day7_a/main.py
@@ -1,64 +1,100 @@
 from os import path
-from collections import defaultdict
 
+OPERATORS = ["OR", "AND", "NOT", "LSHIFT", "RSHIFT"]
+BITS = 16
 
-def get_instructions():
+def parse_instruction(line: str):
+    contents = line.strip().split(" ")
+
+    return contents[:-2], contents[-1]
+
+def get_circuit():
     parent = path.abspath(path.dirname(__file__))
 
     with open(path.join(parent, "puzzle.txt")) as puzzle:
         lines = puzzle.readlines()
 
-    return [line.strip().split(" ") for line in lines]
+    return [parse_instruction(line) for line in lines]
 
+def is_gate(instruction):
+    return len(instruction[0]) > 1
+
+def can_signal(params, out, state):
+    if out in state:
+        return False
+
+    for item in params:
+        if item in OPERATORS:
+            continue
+
+        if item.isdigit():
+            continue
+
+        if item not in state:
+            return False
+
+    return True
+
+def transform_params(params, state):
+    transformed = []
+
+    for param in params:
+        if param in OPERATORS:
+            transformed.append(param)
+        elif param.isdigit():
+            transformed.append(int(param))
+        else:
+            transformed.append(state[param])
+
+    return transformed
+
+def get_signal(params, state):
+    params = transform_params(params, state)
+
+    if "OR" in params:
+        return params[0] | params[2]
+
+    if "AND" in params:
+        return params[0] & params[2]
+
+    if "NOT" in params:
+        # as ~ is with signed integer....
+        return (1 << BITS) - 1 - params[1]
+
+    if "LSHIFT" in params:
+       return params[0] << params[2]
+
+    if "RSHIFT" in params:
+        return params[0] >> params[2]
+
+    return params[0]
+
+def run_circuit(circuit, state):
+    count = len(circuit)
+
+    while count != 0:
+        for params, out in circuit:
+            if not can_signal(params, out, state):
+                continue
+
+            count -= 1
+
+            state[out] = get_signal(params, state)
+
+    return state
+
+def get_signal_from_wire_a(circuit):
+    state = run_circuit(circuit, dict())
+
+    return state["a"]
 
 def main():
-    instructions = get_instructions()
+    circuit = get_circuit()
 
-    registers: defaultdict[str, int] = defaultdict(lambda: 0)
+    a = get_signal_from_wire_a(circuit)
 
-    registers = do_instructions(instructions, registers)
+    print(a)
 
-    print(registers["a"])
-    # print(sorted(registers.items(), key=lambda item: item[0]))
-
-def do_instructions(instructions, registers):
-    for instruction in instructions:
-        registers = do_instruction(instruction, registers)
-
-    return registers
-
-def do_instruction(instruction: list[str], registers):
-    result = 0
-
-    parameters = [resolve_parameter(parameter, registers) for parameter in instruction]
-
-    if instruction[0].isnumeric() and not instruction[1].isalpha():
-        result = int(instruction[0])
-
-    if instruction[1] == "AND":
-        result = parameters[0] & parameters[2]
-
-    if instruction[1] == "OR":
-        result = parameters[0] | parameters[2]
-
-    if instruction[1] == "LSHIFT":
-        result = parameters[0] << parameters[2]
-
-    if instruction[1] == "RSHIFT":
-        result = parameters[0] >> parameters[2]
-
-    if instruction[0] == "NOT":
-        result = ~parameters[1]
-
-    registers[instruction[-1]] = result % 65536
-
-    return registers
-
-def resolve_parameter(parameter: str, registers):
-    if parameter.isnumeric():
-        return int(parameter)
-
-    return registers[parameter]
 
 if __name__ == "__main__":
     main()
--- 2015/day7_b/main.py
+++ 2015/day7_b/main.py
@@ -1,14 +1,103 @@
 from os import path
 
-def get_():
+OPERATORS = ["OR", "AND", "NOT", "LSHIFT", "RSHIFT"]
+BITS = 16
+
+def parse_instruction(line: str):
+    contents = line.strip().split(" ")
+
+    return contents[:-2], contents[-1]
+
+def get_circuit():
     parent = path.abspath(path.dirname(__file__))
 
     with open(path.join(parent, "puzzle.txt")) as puzzle:
-        pass
+        lines = puzzle.readlines()
 
+    return [parse_instruction(line) for line in lines]
+
+def is_gate(instruction):
+    return len(instruction[0]) > 1
+
+def can_signal(params, out, state):
+    if out in state:
+        return False
+
+    for item in params:
+        if item in OPERATORS:
+            continue
+
+        if item.isdigit():
+            continue
+
+        if item not in state:
+            return False
+
+    return True
+
+def transform_params(params, state):
+    transformed = []
+
+    for param in params:
+        if param in OPERATORS:
+            transformed.append(param)
+        elif param.isdigit():
+            transformed.append(int(param))
+        else:
+            transformed.append(state[param])
+
+    return transformed
+
+def get_signal(params, state):
+    params = transform_params(params, state)
+
+    if "OR" in params:
+        return params[0] | params[2]
+
+    if "AND" in params:
+        return params[0] & params[2]
+
+    if "NOT" in params:
+        # as ~ is with signed integer....
+        return (1 << BITS) - 1 - params[1]
+
+    if "LSHIFT" in params:
+       return params[0] << params[2]
+
+    if "RSHIFT" in params:
+        return params[0] >> params[2]
+
+    return params[0]
+
+def run_circuit(circuit, state):
+    while True:
+        done = True
+
+        for params, out in circuit:
+            if not can_signal(params, out, state):
+                continue
+
+            done = False
+
+            state[out] = get_signal(params, state)
+
+        if done:
+            break
+
+    return state
+
+def get_signal_from_wire_a_after_b_reset(circuit):
+    state = run_circuit(circuit, dict())
+    state = run_circuit(circuit, { "b": state["a"] })
+
+    return state["a"]
 
 def main():
-    pass
+    circuit = get_circuit()
+
+    a = get_signal_from_wire_a_after_b_reset(circuit)
+
+    print(a)
 
 
 if __name__ == "__main__":
