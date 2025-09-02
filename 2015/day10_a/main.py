1573e1aeb458af3576b8411a9f4020f74837d679
semroeten <sem.roeten@gmail.com>
2025-09-02 16:53:32 +0200

    day 10

--- 2015/day10_a/main.py
+++ 2015/day10_a/main.py
@@ -1,14 +1,49 @@
 from os import path
 
-def get_():
+ITERATIONS = 40
+
+def get_digits():
     parent = path.abspath(path.dirname(__file__))
 
     with open(path.join(parent, "puzzle.txt")) as puzzle:
-        pass
+        digits = puzzle.read().strip()
 
+    return digits
+
+def next_sequence(digits):
+    sequence = ""
+    last_seen = None
+    count = 0
+
+    for char in digits:
+        if last_seen is None or char == last_seen:
+            last_seen = char
+            count += 1
+            continue
+
+        sequence += f"{count}{last_seen}"
+
+        last_seen = char
+        count = 1
+
+    sequence += f"{count}{last_seen}"
+
+    return sequence
+
+def get_length_of_sequence(digits):
+    sequence = digits
+
+    for i in range(ITERATIONS):
+        sequence = next_sequence(sequence)
+
+    return len(sequence)
 
 def main():
-    pass
+    digits = get_digits()
+
+    length = get_length_of_sequence(digits)
+
+    print(length)
 
 
 if __name__ == "__main__":
